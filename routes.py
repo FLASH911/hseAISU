4f45313d0365ee5ee8f385e46a1d1b66d79efcd2
FLASH911 <oleg@zedpost.ru>
2019-12-04 20:59:34 +0500

    test

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from models import Men
 
 api = Blueprint('api', __name__)
 
 @api.route('/')
 def hello():
-    return 'hello world'
+    return jsonify([ (lambda men: men.json()) (men) for men in Men.query.all()])
