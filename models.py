d154639ca19b4eec7cd7ddc57843b60e7197bdd4
FLASH911 <oleg@zedpost.ru>
2019-12-04 21:23:35 +0500

    test

--- models.py
+++ models.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 
 class Men(db.Model):
-    __tablename__='mens'
+    __tablename__ = 'mens'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    job = db.Column(db.String(120))
 
     def json(self):
-        return {"id": self.id, "name": self.name}
+        return {"id": self.id, "name": self.name, "job": self.job, }
--- routes.py
+++ routes.py
@@ -6,16 +6,16 @@ api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/mens')
 def get_mens():
-    return jsonify([ (lambda men: men.json()) (men) for men in Men.query.all()])
+    return jsonify([(lambda men: men.json()) (men) for men in Men.query.all()])
 
 @api.route('/men/id/<int:men_id>')
 def get_men (men_id):
     men = Men.query.get(men_id)
     return men.json() if men else ''
 
-@api.route('/men/name/<string:men_name>')
-def put_men (men_name):
-    men = Men(name=men_name)
-    db.session.add(men)
+@api.route('/men/name/<string:men_name>;<string:men_job>')
+def put_men (men_name, men_job):
+    addVars = Men(name=men_name, job=men_job)
+    db.session.add(addVars)
     db.session.commit()
-    return jsonify(men.json())
+    return jsonify(addVars.json())
