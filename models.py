c23b53d47d14ed29eedc3d317c42df260f821a7e
FLASH911 <oleg@zedpost.ru>
2019-12-05 20:05:36 +0500

    20:05 051219

--- models.py
+++ models.py
@@ -1,13 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 
-class Men(db.Model):
-    __tablename__ = 'mens'
+class Employee(db.Model):
+    __tablename__ = 'Employee'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    job = db.Column(db.String(120))
+    position_id = db.Column(db.Integer, ForeignKey('Position.id'))
+    position = relationship('Position')
 
     def json(self):
-        return {"id": self.id, "name": self.name, "job": self.job, }
+        return {"id": self.id, "name": self.name, "position": self.position.json()}
+
+class Position(db.Model):
+    __tablename__ = 'Position'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    #job = db.Column(db.String(120))
+
+    def json(self):
+        return {"id": self.id, "name": self.name}
+
--- routes.py
+++ routes.py
@@ -1,22 +1,26 @@
 from flask import Blueprint, jsonify
 
-from models import Men, db
+from models import Employee, db
 
 api = Blueprint('api', __name__, url_prefix='/api')
 index = Blueprint('index', __name__, url_prefix='/')
 
-@api.route('/mens')
-def get_mens():
-    return jsonify([(lambda men: men.json()) (men) for men in Men.query.all()])
+@api.route('/employee')
+def get_employee():
+    return jsonify([(lambda employee: employee.json()) (employee) for employee in Employee.query.all()])
+
+#@api.route('/position')
+#def get_position():
+   # return jsonify([(lambda men: men.json()) (men) for men in position.query.all()])
 
 @api.route('/men/id/<int:men_id>')
 def get_men (men_id):
-    men = Men.query.get(men_id)
+    men = Employee.query.get(men_id)
     return men.json() if men else ''
 
 @api.route('/men/name/<string:men_name>;<string:men_job>')
 def put_men (men_name, men_job):
-    addVars = Men(name=men_name, job=men_job)
+    addVars = Employee(name=men_name, job=men_job)
     db.session.add(addVars)
     db.session.commit()
     return jsonify(addVars.json())
@@ -33,7 +37,7 @@ def get_index():
             <div style="background: green; text-align:center;">
                 <h1 style="font-style:italic; color: red">Hello!</h1>
                 <h3>API</h3>
-                <a href="./api/mens">Mens</a>
+                <a href="./api/employee">Employee</a>
             </div>
         </body>
     </html>
