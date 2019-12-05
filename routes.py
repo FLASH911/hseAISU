6150c7beeef87e724aaa731dd6450a3222787d19
FLASH911 <oleg@zedpost.ru>
2019-12-05 19:34:54 +0500

    19:34 051219

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify
 from models import Men, db
 
 api = Blueprint('api', __name__, url_prefix='/api')
+index = Blueprint('index', __name__, url_prefix='/')
 
 @api.route('/mens')
 def get_mens():
@@ -19,3 +20,19 @@ def put_men (men_name, men_job):
     db.session.add(addVars)
     db.session.commit()
     return jsonify(addVars.json())
+
+@index.route('/')
+@index.route('/index')
+def get_index():
+    return '''
+    <html>
+        <head>
+            <title>Gorshkov - app</title>
+        </head>
+        <body>
+            <h1>Hello!</h1>
+            <h3>API</h3>
+            <a href="./api/mens">Mens</a>
+        </body>
+    </html>
+           '''
